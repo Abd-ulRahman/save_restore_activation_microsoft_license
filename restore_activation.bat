d000013feffe3acffe81da7cf0ff2401e8c218ae
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2025-06-07 13:24:32 -0700

    Add files via upload

--- restore_activation.bat
+++ restore_activation.bat
@@ -1,3 +1,4 @@
+set ScriptPath=%~dp0
 rem Offline script to save and restore activation for Microsoft Windows and Office licenses by Thelogh
 rem Backup and Restore Microsoft Windows and Microsoft Office License Activation Offline
 rem https://www.alldiscoveries.com/backup-and-restore-microsoft-windows-and-microsoft-office-license-activation-offline/
@@ -8,9 +9,9 @@ rem V.1.0.0
 
 net stop clipsvc
 net stop sppsvc
-xcopy .\license\2.0\ C:\Windows\System32\spp\store\2.0 /O /X /E /H /K /Y
-xcopy .\GenuineTicket\tokens.dat C:\ProgramData\Microsoft\Windows\ClipSVC /O /X /H /K /Y
-copy /Y .\winactivationkey.reg C:\Windows\System32\winactivationkey.reg
-psexec -i -s -accepteula reg import C:\Windows\System32\winactivationkey.reg
+xcopy "%ScriptPath%license\2.0\" C:\Windows\System32\spp\store\2.0 /O /X /E /H /K /Y
+xcopy "%ScriptPath%GenuineTicket\tokens.dat" C:\ProgramData\Microsoft\Windows\ClipSVC /O /X /H /K /Y
+copy /Y "%ScriptPath%winactivationkey.reg" C:\Windows\System32\winactivationkey.reg
+"%ScriptPath%psexec" -i -s -accepteula reg import C:\Windows\System32\winactivationkey.reg
 del C:\Windows\System32\winactivationkey.reg
 shutdown /r /t 0
--- save_activation.bat
+++ save_activation.bat
@@ -1,3 +1,4 @@
+set ScriptPath=%~dp0
 rem Offline script to save and restore activation for Microsoft Windows and Office licenses by Thelogh
 rem Backup and Restore Microsoft Windows and Microsoft Office License Activation Offline
 rem https://www.alldiscoveries.com/backup-and-restore-microsoft-windows-and-microsoft-office-license-activation-offline/
@@ -6,16 +7,22 @@ rem REPOSITORY
 rem https://github.com/thelogh/save_restore_activation_microsoft_license
 rem V.1.0.0
 
-net stop clipsvc
-net stop sppsvc
-xcopy C:\Windows\System32\spp\store .\license /O /X /E /H /K /Y
+cmd /v:on /c md "%ScriptPath%license"
+
+cmd /v:on /c md "%ScriptPath%GenuineTicket"
 
 net stop clipsvc
 net stop sppsvc
-xcopy C:\ProgramData\Microsoft\Windows\ClipSVC\tokens.dat .\GenuineTicket /O /X /H /K /Y
+cmd /v:on /c XCOPY C:\Windows\System32\spp\store "%ScriptPath%license" /O /X /E /H /K /Y
 
-psexec -i -s -accepteula reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\{7746D80F-97E0-4E26-9543-26B41FC22F79} .\winactivationkey.reg
+net stop clipsvc
+net stop sppsvc
+cmd /v:on /c XCOPY C:\ProgramData\Microsoft\Windows\ClipSVC\tokens.dat "%ScriptPath%GenuineTicket" /O /X /H /K /Y
 
-copy C:\Windows\System32\winactivationkey.reg .\
+"%ScriptPath%psexec" -i -s -accepteula reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\{7746D80F-97E0-4E26-9543-26B41FC22F79} .\winactivationkey.reg
 
-del C:\Windows\System32\winactivationkey.reg
+cmd /v:on /c copy C:\Windows\System32\winactivationkey.reg "%ScriptPath%"
+
+cmd /v:on /c del C:\Windows\System32\winactivationkey.reg
+
+pause
