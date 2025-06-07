726268cd58e71f2e033626368dca63eae8ba5f70
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2025-06-08 00:39:16 +0300

    Update save_activation.bat

--- save_activation.bat
+++ save_activation.bat
@@ -7,22 +7,26 @@ rem REPOSITORY
 rem https://github.com/thelogh/save_restore_activation_microsoft_license
 rem V.1.0.0
 
-cmd /v:on /c md "%ScriptPath%license"
 
-cmd /v:on /c md "%ScriptPath%GenuineTicket"
+rem Create directories for placing the license and token file.
+if not exist "%ScriptPath%license" cmd /v:on /c md "%ScriptPath%license"
+if not exist "%ScriptPath%GenuineTicket" cmd /v:on /c md "%ScriptPath%GenuineTicket"
 
+rem Stop the services to get the license and token file
 net stop clipsvc
 net stop sppsvc
 cmd /v:on /c XCOPY C:\Windows\System32\spp\store "%ScriptPath%license" /O /X /E /H /K /Y
-
-net stop clipsvc
-net stop sppsvc
 cmd /v:on /c XCOPY C:\ProgramData\Microsoft\Windows\ClipSVC\tokens.dat "%ScriptPath%GenuineTicket" /O /X /H /K /Y
 
-"%ScriptPath%psexec" -i -s -accepteula reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\{7746D80F-97E0-4E26-9543-26B41FC22F79} .\winactivationkey.reg
+rem Dump the licnese in registry 
+%ScriptPath%psexec -i -s -accepteula reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\{7746D80F-97E0-4E26-9543-26B41FC22F79} .\winactivationkey.reg
 
-cmd /v:on /c copy C:\Windows\System32\winactivationkey.reg "%ScriptPath%"
-
+cmd /v:on /c copy C:\Windows\System32\winactivationkey.reg %ScriptPath%
 cmd /v:on /c del C:\Windows\System32\winactivationkey.reg
 
+rem Bring back the services
+net start clipsvc
+net start sppsvc
+
+echo Your licnese file is located at .\At the same folder, please keep it safe.
 pause
