1ee34b3838f380bf47ac2e35a16cd161f424d84e
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2025-06-27 01:56:45 -0700

    Add files via upload

---
+++ Old WA/restore_activation.bat
@@ -0,0 +1,38 @@
+set ScriptPath=%~dp0
+rem Offline script to save and restore activation for Microsoft Windows and Office licenses by Thelogh
+rem Backup and Restore Microsoft Windows and Microsoft Office License Activation Offline
+rem https://www.alldiscoveries.com/backup-and-restore-microsoft-windows-and-microsoft-office-license-activation-offline/
+rem For all requests write on the blog
+rem REPOSITORY
+rem https://github.com/thelogh/save_restore_activation_microsoft_license
+rem V.1.0.0
+
+echo Do not connect your PC or SERVER to the internet until the license has been restored!!!
+echo Please make sure the internet is disconnected before you continue.
+pause
+
+rem Check the backup directory exist or not
+if not exist "%ScriptPath%license" echo The 'license' directory is missing. && goto end
+if not exist "%ScriptPath%GenuineTicket" echo The 'GenuineTicket' directory is missing. && goto end
+if not exist "%ScriptPath%winactivationkey.reg" echo File 'winactivationkey.reg' is missing && goto end
+
+rem The files are ready, go on to restore license(s)
+
+rem Stop the services
+net stop clipsvc
+net stop sppsvc
+
+
+XCOPY %ScriptPath%license\2.0\ C:\Windows\System32\spp\store\2.0 /O /X /E /H /K /Y
+XCOPY %ScriptPath%GenuineTicket\tokens.dat C:\ProgramData\Microsoft\Windows\ClipSVC /O /X /H /K /Y
+copy /Y %ScriptPath%winactivationkey.reg C:\Windows\System32\winactivationkey.reg
+%ScriptPath%psexec -i -s -accepteula reg import C:\Windows\System32\winactivationkey.reg
+del C:\Windows\System32\winactivationkey.reg
+
+echo Restoring license is finished. You may need to check whether there is some errors in the output above.
+echo You computer will reboot after you press any key.
+pause >nul
+shutdown /r /t 0
+
+:end 
+echo Please check make sure the backup directory is existed and retry by running this programme again.
