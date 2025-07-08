e2757d5880e78cec60c871ae4b5a27f43efb2022
Abdulrahman Alkhamees <aaalkhamees@outlook.com>
2025-07-08 20:02:00 +0300

    Add files via upload

--- Old_WA/restore_activation.bat
+++ Old_WA/restore_activation.bat
@@ -1,4 +1,9 @@
-set ScriptPath=%~dp0
+@echo off
+setlocal enabledelayedexpansion
+
+:: Set backup directory
+set "ScriptPath=%~dp0"
+set "backupDir=%~dp0ActivationBackup"
 rem Offline script to save and restore activation for Microsoft Windows and Office licenses by Thelogh
 rem Backup and Restore Microsoft Windows and Microsoft Office License Activation Offline
 rem https://www.alldiscoveries.com/backup-and-restore-microsoft-windows-and-microsoft-office-license-activation-offline/
@@ -12,9 +17,9 @@ echo Please make sure the internet is disconnected before you continue.
 pause
 
 rem Check the backup directory exist or not
-if not exist "%ScriptPath%license" echo The 'license' directory is missing. && goto end
-if not exist "%ScriptPath%GenuineTicket" echo The 'GenuineTicket' directory is missing. && goto end
-if not exist "%ScriptPath%winactivationkey.reg" echo File 'winactivationkey.reg' is missing && goto end
+if not exist "!backupDir!\license" echo The 'license' directory is missing. && goto end
+if not exist "!backupDir!\GenuineTicket" echo The 'GenuineTicket' directory is missing. && goto end
+if not exist "!backupDir!\winactivationkey.reg" echo File 'winactivationkey.reg' is missing && goto end
 
 rem The files are ready, go on to restore license(s)
 
@@ -23,11 +28,15 @@ net stop clipsvc
 net stop sppsvc
 
 
-XCOPY %ScriptPath%license\2.0\ C:\Windows\System32\spp\store\2.0 /E /H /C /I /Q /Y
-cmd /v:on /c XCOPY %ScriptPath%GenuineTicket\tokens.dat %ProgramData%\Microsoft\Windows\ClipSVC /O /X /H /K /Y
-cmd /v:on /c copy /Y %ScriptPath%winactivationkey.reg %SystemRoot%\System32\winactivationkey.reg
-%ScriptPath%psexec -i -s -accepteula reg import %SystemRoot%\System32\winactivationkey.reg
-cmd /v:on /c del %SystemRoot%\System32\winactivationkey.reg
+XCOPY !backupDir!\license\2.0\ C:\Windows\System32\spp\store\2.0 /E /H /C /I /Q /Y
+
+XCOPY !backupDir!\GenuineTicket\tokens.dat %ProgramData%\Microsoft\Windows\ClipSVC /O /X /H /K /Y
+
+copy /Y !backupDir!\winactivationkey.reg %SystemRoot%\System32\winactivationkey.reg
+
+"!ScriptPath!\PsExec.exe" -i -s -accepteula reg import %SystemRoot%\System32\winactivationkey.reg
+
+del %SystemRoot%\System32\winactivationkey.reg
 
 echo Restoring license is finished. You may need to check whether there is some errors in the output above.
 echo You computer will reboot after you press any key.
--- Old_WA/save_activation.bat
+++ Old_WA/save_activation.bat
@@ -1,5 +1,9 @@
 @echo off
-set ScriptPath=%~dp0
+setlocal enabledelayedexpansion
+
+:: Set paths for backup and logs
+set "ScriptPath=%~dp0"
+set "backupDir=%~dp0ActivationBackup"
 rem Offline script to save and restore activation for Microsoft Windows and Office licenses by Thelogh
 rem Backup and Restore Microsoft Windows and Microsoft Office License Activation Offline
 rem For all requests write on the blog
@@ -9,24 +13,28 @@ rem V.1.0.0
 
 
 rem Create directories for placing the license and token file.
-if not exist "%ScriptPath%license" mkdir "%ScriptPath%license"
-if not exist "%ScriptPath%GenuineTicket" mkdir "%ScriptPath%GenuineTicket"
+:: Create backup directory
+if not exist "!backupDir!" mkdir "!backupDir!"
+if not exist "!backupDir!\license" mkdir "!backupDir!\license"
+if not exist "!backupDir!\GenuineTicket" mkdir "!backupDir!\GenuineTicket"
 
 rem Stop the services to get the license and token file
 net stop clipsvc
 net stop sppsvc
-XCOPY %SystemRoot%\System32\spp\store "%ScriptPath%license" /E /H /C /I /Q /Y
-cmd /v:on /c XCOPY %ProgramData%\Microsoft\Windows\ClipSVC\tokens.dat "%ScriptPath%GenuineTicket" /O /X /H /K /Y
+
+XCOPY %SystemRoot%\System32\spp\store "!backupDir!\license" /E /H /C /I /Q /Y
+
+XCOPY %ProgramData%\Microsoft\Windows\ClipSVC\tokens.dat "!backupDir!\GenuineTicket" /O /X /H /K /Y
 
 rem Dump the licnese in registry 
-%ScriptPath%psexec -i -s -accepteula reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\{7746D80F-97E0-4E26-9543-26B41FC22F79} .\winactivationkey.reg
+"!ScriptPath!\PsExec.exe" -i -s -accepteula reg export HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\{7746D80F-97E0-4E26-9543-26B41FC22F79} .\winactivationkey.reg
 
-cmd /v:on /c copy %SystemRoot%\System32\winactivationkey.reg %ScriptPath%
-cmd /v:on /c del %SystemRoot%\System32\winactivationkey.reg
+copy %SystemRoot%\System32\winactivationkey.reg !backupDir!
+del %SystemRoot%\System32\winactivationkey.reg
 
 rem Bring back the services
 net start clipsvc
 net start sppsvc
 
-echo Your licnese file is located at .\At the same folder, please keep it safe.
+echo Your licnese file is located at: "!backupDir!", please keep it safe.
 pause
